6a929759bb5fdecf6f9ab5cfb70f6ad696971455
sumanth-08 <mrsumanth1947@gmail.com>
2024-03-11 15:21:57 +0530

    edit profile fixes

--- src/controllers/authentications/editProfile.js
+++ src/controllers/authentications/editProfile.js
@@ -2,21 +2,54 @@ import { Router } from "express";
 import { send, setErrorResponseMsg } from "../../helper/responseHelper.js";
 import RESPONSE from "../../configs/global.js";
 import accountsModel from "../../models/accountsModel.js";
-import bcrypt from "bcrypt";
 import constants from "../../configs/constants.js";
-import jwtTokenCreation from "../../middlewares/jwtTokenCreation.js";
 import authenticate from "../../middlewares/authenticate.js";
 const router = Router();
 
 router.put("/", authenticate, async (req, res) => {
   try {
-    // console.log(req.user.id);
     const { name, email, phone } = req.body;
     const updates = {};
 
     if (name || name != "") updates.name = name;
-    if (email || email != "") updates.email = email;
-    if (phone || phone != "") updates.phone = phone;
+
+    if (email || email != "") {
+      const emailPattern = String(email).match(
+        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+      );
+      if (
+        !emailPattern ||
+        emailPattern.length <= 0 ||
+        email.indexOf(" ") >= 0
+      ) {
+        const updated_response = setErrorResponseMsg(
+          RESPONSE.INVALID_DATA,
+          "Email"
+        );
+        return send(res, updated_response);
+      } else {
+        updates.email = email;
+      }
+    }
+
+    if (phone || phone != "") {
+      const phoneNumberPattern = String(phone).match(/^\+\d{10,15}$/);
+      if (
+        !phoneNumberPattern ||
+        phoneNumberPattern.length <= 0 ||
+        String(phone).indexOf(" ") >= 0
+      ) {
+        const updated_response = setErrorResponseMsg(
+          RESPONSE.INVALID_DATA,
+          "Phone no."
+        );
+        return send(res, updated_response);
+      } else {
+        updates.phone = phone;
+      }
+    }
+
+    await accountsModel.updateMany({ _id: req.user.id }, [{ $set: updates }]);
 
     return send(res, RESPONSE.SUCCESS);
   } catch (err) {
