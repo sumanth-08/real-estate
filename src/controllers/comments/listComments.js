156eedf1240fa109908ce32e8f0becbc3fc601b3
sumanth <mrsumanth1947@gmail.com>
2024-03-20 15:01:41 +0530

    comment and reply list fixes

--- src/controllers/comments/listComments.js
+++ src/controllers/comments/listComments.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import authenticate from "../../middlewares/authenticate.js";
 import commentModel from "../../models/commentModel.js";
+import replyModel from "../../models/cmtRplyModel.js";
 import constants from "../../configs/constants.js";
 import RESPONSE from "../../configs/global.js";
 import { send, setErrorResponseMsg } from "../../helper/responseHelper.js";
@@ -17,25 +18,33 @@ router.get("/", authenticate, async (req, res) => {
       return send(res, updatedResponse);
     }
 
-    // let data = await commentModel.aggregate([
-    //   {
-    //     $match: {
-    //       $expr: {
-    //         $eq: ["$property", { $toObjectId: property_id }],
-    //       },
-    //       isactive: constants.CONTENT_STATE.IS_ACTIVE,
-    //     },
-    //   },
-    //   {
-    //     $lookup: {
-    //       from: "replycomment",
-    //       localField: "comment",
-    //       foreignField: "_id",
-    //       as: "cmammnetInfo",
-    //     },
-    //   },
-    // ]);
-    // return send(res, RESPONSE.SUCCESS, data);
+    let data = await commentModel.aggregate([
+      {
+        $lookup: {
+          from: "replycomments",
+          let: { commentId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ["$comment", "$$commentId"],
+                },
+                isactive: constants.CONTENT_STATE.IS_ACTIVE,
+              },
+            },
+          ],
+          as: "replies",
+        },
+      },
+      {
+        $match: {
+          $expr: { $eq: ["$property", { $toObjectId: property_id }] },
+          isactive: constants.CONTENT_STATE.IS_ACTIVE,
+        },
+      },
+    ]);
+
+    return send(res, RESPONSE.SUCCESS, data);
   } catch (err) {
     console.log(err.message);
     return send(res, RESPONSE.UNKNOWN_ERROR);
