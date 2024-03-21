4bdd642f0e8e4f6f5c377d558b8ed087564f89b3
sumanth <110365705+sumanth-08@users.noreply.github.com>
2024-03-21 11:05:34 +0530

    Delete src/controllers/Admin directory

--- src/controllers/Admin/manageVendors/apiHandler.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import acceptVendorRequest from "./acceptVendorRequest.js";
-import listVendorRequest from "./listVendorRequest.js";
-const router = Router();
-
-router.use("/accept/vendor", acceptVendorRequest);
-router.use("/vendor/list", listVendorRequest);
-
-export default router;
