69006978ae2a13cd0a2512ad67eafeeab809c210
sumanth <mrsumanth1947@gmail.com>
2024-03-22 07:35:44 +0530

    particular property

--- src/controllers/manageProperties/apiHandler.js
+++ src/controllers/manageProperties/apiHandler.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import postProperties from "./postProperties.js";
 import myProperties from "./listProperties.js";
 import deleteProperties from "./deleteProperties.js";
+import particularProperty from "./particularProperty.js";
 const router = Router();
 
 router.use("/manage/post", postProperties);
 router.use("/manage/list", myProperties);
 router.use("/manage/delete", deleteProperties);
+router.use("/manage/list/particular", particularProperty);
 
 export default router;
